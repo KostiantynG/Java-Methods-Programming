56a86e51b0e839a6c2a0065a0e34bff2923b1272
KostiantynG <gorbunovkostiantyn@gmail.com>
2015-11-09 15:16:28 +0200

    Last

--- Java Methods Programming Book Code/src/by/bsu/transformation/StringToInt.java
+++ Java Methods Programming Book Code/src/by/bsu/transformation/StringToInt.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package by.bsu.transformation;
 
 public class StringToInt {
@@ -17,23 +16,4 @@ public class StringToInt {
 			System.err.println("Неверный формат числа " + e);
 		}
 	}
-=======
-package by.bsu.transformation;
-
-public class StringToInt {
-	public static void main(String[] args) {
-		String arg = "71"; // 071 или 0x71или 0b1000111
-		try {
-			int value1 = Integer.parseInt(arg); // возвращает int
-			int value2 = Integer.valueOf(arg); // возвращает Integer
-			int value3 = Integer.decode(arg); // возвращает Integer
-			int value4 = new Integer(arg); /*
-											 * создает Integer, для
-											 * преобразования применяется редко
-											 */
-		} catch (NumberFormatException e) {
-			System.err.println("Неверный формат числа " + e);
-		}
-	}
->>>>>>> origin/master
 }
