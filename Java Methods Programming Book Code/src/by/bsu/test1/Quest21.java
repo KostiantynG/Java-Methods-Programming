573390f4b6bb09908291cdf704b70d37dcefe4b2
KostiantynG <gorbunovkostiantyn@gmail.com>
2016-05-19 17:06:12 +0300

    Change

---
+++ Java Methods Programming Book Code/src/by/bsu/test1/Quest21.java
@@ -0,0 +1,9 @@
+package by.bsu.test1;
+
+/* Код класса Quest21 ошибок не содержит*/
+public class Quest21 {
+
+    public static void main (String [] args) {
+        System.out.println ("Hello, java 7");
+    }
+}
