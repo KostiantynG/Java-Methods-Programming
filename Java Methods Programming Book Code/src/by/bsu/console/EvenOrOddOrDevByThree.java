ae079e223e718a41f8136df8932691784254cdf6
KostiantynG <gorbunovkostiantyn@gmail.com>
2017-06-08 16:27:17 +0300

    Add new Classes

---
+++ Java Methods Programming Book Code/src/by/bsu/console/EvenOrOddOrDevByThree.java
@@ -0,0 +1,17 @@
+package by.bsu.console;
+
+public class EvenOrOddOrDevByThree {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i == 0) {
+                System.out.println(i + " even");
+            } else if (i % 3 == 0) {
+                System.out.println(i + " divided by 3");
+            } else if (i % 2 == 0) {
+                System.out.println(i + " even");
+            } else {
+                System.out.println(i + "  odd");
+            }
+        }
+    }
+}
---
+++ Java Methods Programming Book Code/src/by/bsu/console/WordCounter.java
@@ -0,0 +1,21 @@
+package by.bsu.console;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCounter {
+    public static void main(String[] args) {
+        String string = "Hello Word my name is Z hello all";
+        String[] split = string.toLowerCase().split(" ");
+        Map<String, Integer> words = new HashMap<>();
+        Integer value = 1;
+        for (String s : split) {
+            if (words.get(s) == null) {
+                words.put(s, value);
+            } else {
+                words.replace(s, value + 1);
+            }
+        }
+        System.out.println(words);
+    }
+}
